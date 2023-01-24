1fa0f13eeeedb212229949a7ff6dd326939008e0
Omer Dolev <odolev@microsoft.com>
2023-01-24 14:06:11 +0200

    fix build

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ export function useFormErrors(errors?: Record<string, string>) {
 			}
 		) {
 			function applyCustomValidity(el: HTMLInputElement | HTMLTextAreaElement) {
-				el.setCustomValidity(validate(el, new FormData(el.form?.cloneNode() ?? undefined)));
+				el.setCustomValidity(validate(el, new FormData(el.form?.cloneNode() as HTMLFormElement ?? undefined)));
 
 				if (el.validity.customError) {
 					return;
